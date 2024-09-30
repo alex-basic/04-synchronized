6f4afa2620da7930f3a1bb1b546618a1587a44ac
Alex Poletavkin <poletavin.av@gmail.com>
2024-09-30 14:10:40 +0200

    added two monitors

--- src/ait/elevator/task/Truck.java
+++ src/ait/elevator/task/Truck.java
@@ -3,7 +3,7 @@ package ait.elevator.task;
 import ait.elevator.model.Elevator;
 
 public class Truck implements Runnable {
-    private static Object monitor = new Object();
+    private static Object[] monitor = {new Object(), new Object()};
     private int nRaces;
     private int capacity;
     private Elevator[] elevators;
@@ -17,8 +17,8 @@ public class Truck implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < nRaces; i++) {
-            synchronized (monitor) {
-                for (int j = 0; j < elevators.length; j++) {
+            for (int j = 0; j < elevators.length; j++) {
+                synchronized (monitor[j]) {
                     elevators[j].add(capacity/elevators.length);
                 }
             }
